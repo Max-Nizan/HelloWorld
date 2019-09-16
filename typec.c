c45ef6363a1fd9d84080b9d5279d21226ec44ac6
Max-Nizan <51700396+Max-Nizan@users.noreply.github.com>
2019-09-16 22:40:42 +0300

    код работает корректно в visual studio

--- typec.c
+++ typec.c
@@ -1,12 +1,21 @@
-// демонстрация типов в си. их вывод
+// демонстрация типов в си. 
 #include <stdio.h>
+#include <inttypes.h>
+#include <locale.h>
 
-int main()
+int main(void)
 {
-    int a=2147483647;
-    float fl;
+	setlocale(LC_ALL, "RU");
+	int32_t m32; // 32бит переменная со знаком
+	int a = 2147483647;
+	m32 = 45933945;
+	printf("предположим что m32 int: ");
+	printf("m32=%d\n", m32);
+	printf("теперь используем макрос: ");
+	printf("m32=%" PRId32 "\n", m32);
 
-printf("Целочисленно переполнение %d +1 %d  +2  %d\n", a, a+1, a+2);
+	//
+	printf("Целочисленно переполнение %d +1 %d  +2  %d\n", a, a + 1, a + 2);
 
     return 0;
 }
